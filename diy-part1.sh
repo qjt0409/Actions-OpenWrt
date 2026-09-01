48042a2fcd367bc9faad3b48d7f4111ddb0ca16e
qjt0409 <50192530+qjt0409@users.noreply.github.com>
2026-09-01 15:11:59 +0800

    Add software sources to feeds.conf.default
    Added multiple software sources to feeds.conf.default using sed commands.

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,17 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#!/bin/bash
+# 添加软件源 feeds
+sed -i '1i src-git istore https://github.com/istoreos/istoreos-feeds.git;main' feeds.conf.default
+sed -i '1i src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
+sed -i '1i src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' feeds.conf.default
+sed -i '1i src-git lucky https://github.com/gdy666/luci-app-lucky.git;main' feeds.conf.default
+sed -i '1i src-git ddnsgo https://github.com/sirpdboy/luci-app-ddns-go.git' feeds.conf.default
+sed -i '1i src-git push https://github.com/zzsj001/luci-app-pushd.git' feeds.conf.default
+sed -i '1i src-git wechatpush https://github.com/tty228/luci-app-wechatpush.git' feeds.conf.default
+sed -i '1i src-git alist https://github.com/sirpdboy/luci-app-alist.git' feeds.conf.default
+sed -i '1i src-git easycloud https://github.com/linkease/istore-packages.git' feeds.conf.default
+sed -i '1i src-git turboacc https://github.com/chenmozhijin/turboacc.git' feeds.conf.default
+
+./scripts/feeds update -a
