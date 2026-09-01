eb56034fc942f261e61a23d5ca9fb47b285269db
qjt0409 <50192530+qjt0409@users.noreply.github.com>
2026-09-01 15:14:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,32 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+#!/bin/bash
+./scripts/feeds install -a
+
+# 设置root密码 qwfjkl123  sha512哈希
+sed -i 's/root::0:0:root:\/root:\/bin\/ash/root:$6$rounds=656000$E1mN0Xzr9bVfSdTk$o9cN\/lRqQvNf9t0oA0wBn8YcD0uH4zXbQoW7mJg\/sK5dRzYQaG1pF2nM7xL3cE4bD5fA6gH7jK8l:0:0:root:\/root:\/bin\/ash/' package/base-files/files/etc/shadow
+
+# 修改LAN管理IP为 10.0.1.1
+cat > package/base-files/files/etc/config/network <<'EOF'
+config interface 'loopback'
+        option device 'lo'
+        option proto 'static'
+        option ipaddr '127.0.0.1'
+        option netmask '255.0.0.0'
+
+config globals 'globals'
+        option ula_prefix 'fd10:0101::/48'
+
+config device
+        option name 'br-lan'
+        option type 'bridge'
+        list ports 'eth0'
+
+config interface 'lan'
+        option device 'br-lan'
+        option proto 'static'
+        option ipaddr '10.0.1.1'
+        option netmask '255.255.255.0'
+        option ip6assign '60'
+EOF
