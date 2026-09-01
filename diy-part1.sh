32bebf6b63bc98b7c5c961495882781e866b3b61
qjt0409 <50192530+qjt0409@users.noreply.github.com>
2026-09-01 15:33:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,23 +10,29 @@
 # See /LICENSE for more information.
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# ========== 清理全部旧的src‑git源，避免重复报错 ==========
+sed -i '/^src-git /d' feeds.conf.default
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#!/bin/bash
-# 添加软件源 feeds
-sed -i '1i src-git istore https://github.com/istoreos/istoreos-feeds.git;main' feeds.conf.default
-sed -i '1i src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
-sed -i '1i src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' feeds.conf.default
-sed -i '1i src-git lucky https://github.com/gdy666/luci-app-lucky.git;main' feeds.conf.default
-sed -i '1i src-git ddnsgo https://github.com/sirpdboy/luci-app-ddns-go.git' feeds.conf.default
-sed -i '1i src-git push https://github.com/zzsj001/luci-app-pushd.git' feeds.conf.default
-sed -i '1i src-git wechatpush https://github.com/tty228/luci-app-wechatpush.git' feeds.conf.default
-sed -i '1i src-git alist https://github.com/sirpdboy/luci-app-alist.git' feeds.conf.default
-sed -i '1i src-git easycloud https://github.com/linkease/istore-packages.git' feeds.conf.default
-sed -i '1i src-git turboacc https://github.com/chenmozhijin/turboacc.git' feeds.conf.default
+# 函数：仅当源不存在时才添加，防止重复
+add_feed(){
+    local name="$1"
+    local line="$2"
+    if ! grep -q "^src-git $name" feeds.conf.default;then
+        echo "$line" >> feeds.conf.default
+    fi
+}
+
+# 增加需要的插件源
+add_feed "helloworld"    "src-git helloworld https://github.com/fw876/helloworld"
+add_feed "istore"        "src-git istore https://github.com/istoreos/istoreos-feeds.git;main"
+add_feed "passwall"      "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main"
+add_feed "passwall2"     "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main"
+add_feed "lucky"         "src-git lucky https://github.com/gdy666/luci-app-lucky.git;main"
+add_feed "ddnsgo"        "src-git ddnsgo https://github.com/sirpdboy/luci-app-ddns-go.git"
+add_feed "push"          "src-git push https://github.com/zzsj001/luci-app-pushd.git"
+add_feed "wechatpush"    "src-git wechatpush https://github.com/tty228/luci-app-wechatpush.git"
+add_feed "alist"         "src-git alist https://github.com/sirpdboy/luci-app-alist.git"
+add_feed "easycloud"     "src-git easycloud https://github.com/linkease/istore-packages.git"
+add_feed "turboacc"      "src-git turboacc https://github.com/chenmozhijin/turboacc.git"
 
 ./scripts/feeds update -a
